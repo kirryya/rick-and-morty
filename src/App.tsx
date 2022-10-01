463b72a0d4695a0f3dd1d80bf0c51bbdc759d455
kirryya <kir1987@inbox.ru>
2022-10-01 14:01:59 +0300

    prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,4 @@ import React, { FC } from 'react';
 
 import { ReturnComponentType } from 'types';
 
-export const App: FC = (): ReturnComponentType => {
-  return <div>Rick and Morty</div>;
-};
+export const App: FC = (): ReturnComponentType => <div>Rick and Morty</div>;
