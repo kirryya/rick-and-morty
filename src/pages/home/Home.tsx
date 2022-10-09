20997966b447ead9341fa45d312d2fa3b04b9349
kirryya <kir1987@inbox.ru>
2022-10-09 16:22:39 +0300

    add background image

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,15 @@
 import React, { FC } from 'react';
 
+import fontImage from '../../assets/images/rik-i-morti.jpg';
+
+import style from './Home.module.css';
+
 import { ReturnComponentType } from 'types';
 
 export const Home: FC = (): ReturnComponentType => {
-  return <div>HOME</div>;
+  const font = {
+    backgroundImage: `url(${fontImage})`,
+  };
+
+  return <div className={style.mainBlock} style={font} />;
 };
